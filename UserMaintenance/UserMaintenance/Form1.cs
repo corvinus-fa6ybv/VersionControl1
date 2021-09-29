66b761c736659489d6c260d19a694553ad602719
Boglárka Benkő <boglarka.benko2@stud.uni-corvinus.hu>
2021-09-29 13:38:52 +0200

    saveToFile

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,7 @@ namespace UserMaintenance
 
             label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
+            button2.Text = Resource1.SaveToFile;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -33,6 +35,27 @@ namespace UserMaintenance
                 FullName = textBox1.Text,
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text|*.txt|All|*.*";
+            sfd.DefaultExt = "csv";
+            sfd.AddExtension = true;
+            sfd.FileName = "Lista";
+            if (sfd.ShowDialog() == DialogResult.OK) return;
+
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                foreach (var item in users)
+                {
+                    sw.Write(item.FullName);
+                    sw.Write(item.ID);
+                    sw.WriteLine();
+                }
+            }
+
         }
     }
 }
