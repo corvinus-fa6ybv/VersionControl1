4ba96dbfd8a9202d09977c09b8b78c1f47df9d5c
Boglárka Benkő <boglarka.benko2@stud.uni-corvinus.hu>
2021-11-12 15:34:09 +0100

    Futószalag létrehozása

--- week08/Form1.cs
+++ week08/Form1.cs
@@ -7,14 +7,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week08.Entities;
 
 namespace week08
 {
     public partial class Form1 : Form
     {
+        private List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _factory;
+
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            panel1.Controls.Add(ball);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+            foreach (var item in _balls)
+            {
+                item.MoveBall();
+                if(item.Left > maxPosition)
+                {
+                    maxPosition = item.Left;
+                }
+
+                if (maxPosition == 1000)
+                {
+                    var oldest = _balls[0];
+                    panel1.Controls.Remove(oldest);
+                    _balls.Remove(oldest);
+                }
+            }
         }
     }
 }
