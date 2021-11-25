df0c52121ad37c044bbbd0cfd6e7c86188f01788
Boglárka Benkő <boglarka.benko2@stud.uni-corvinus.hu>
2021-11-25 14:30:46 +0100

    Register függvény Validálás kivétel

--- UnitTestExample1/AccountControllerTestFixture.cs
+++ UnitTestExample1/AccountControllerTestFixture.cs
@@ -1,8 +1,10 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Activities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using UnitTestExample.Controllers;
 
@@ -32,13 +34,65 @@ namespace UnitTestExample.Test
             TestCase("Abcd1234", false),
             TestCase("Abcd1234", true),
             TestCase("abcd1234", false),
-            TestCase("abcdABCD", false),
+            TestCase("abcdABCD", false)
         ]
         public void TestValidatePassword(string password, bool result)
         {
             var accountController = new AccountController();
             var actualResult = accountController.ValidatePassword(password);
             Assert.AreEqual(result, actualResult);
+            
+        }
+
+        [
+            Test,
+            TestCase("irf@uni-corvinus.hu", "Abcd1234"),
+            TestCase("irf@uni-corvinus.hu", "Abcd1234567")
+        ]
+        public void TestRegisterHappyPath(string email, string password)
+        {
+            
+            var accountController = new AccountController();
+
+           
+            var actualResult = accountController.Register(email, password);
+            Assert.AreEqual(email, actualResult.Email);
+            Assert.AreEqual(password, actualResult.Password);
+            Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+        }
+
+        [
+    Test,
+    TestCase("irf@uni-corvinus", "Abcd1234"),
+    TestCase("irf.uni-corvinus.hu", "Abcd1234"),
+    TestCase("irf@uni-corvinus.hu", "abcd1234"),
+    TestCase("irf@uni-corvinus.hu", "ABCD1234"),
+    TestCase("irf@uni-corvinus.hu", "abcdABCD"),
+    TestCase("irf@uni-corvinus.hu", "Ab1234"),
+]
+        public void TestRegisterValidateException(string email, string password)
+        {
+            
+            var accountController = new AccountController();
+
+            
+            try
+            {
+                var actualResult = accountController.Register(email, password);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOf<ValidationException>(ex);
+            }
+
+            // Assert
+        }
+
+        public bool TestValidatePassword1(string password)
+        {
+             var feltetel= new Regex("[a-zA-Z0-9]{8,}");
+            return feltetel.IsMatch(password);
         }
     }
 }
